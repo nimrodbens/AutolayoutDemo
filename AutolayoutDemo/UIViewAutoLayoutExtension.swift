fb610f049111ff50ce4ef96241051f54766d480f
nimrodbens <nimrodbens@gmail.com>
2016-05-20 11:40:23 +0300

    Updated to work with the current Swift version
    Plus added some new constrains functions with multiplier and aligning to top/bottom/left/right

--- AutolayoutDemo/UIViewAutoLayoutExtension.swift
+++ AutolayoutDemo/UIViewAutoLayoutExtension.swift
@@ -8,15 +8,15 @@
 import UIKit
 
 extension NSLayoutConstraint {
-    class func constraintsWithVisualFormat(visualFormat: String, views: [NSObject : AnyObject]) -> [AnyObject] {
-        return NSLayoutConstraint.constraintsWithVisualFormat(visualFormat, options: nil, metrics: nil, views: views)
+    class func constraintsWithVisualFormat(visualFormat: String, views: [String : AnyObject]) -> [AnyObject] {
+        return NSLayoutConstraint.constraintsWithVisualFormat(visualFormat, options: [], metrics: nil, views: views)
     }
     
-    class func constraintsWithVisualFormat(visualFormat: String,  metrics: [NSObject : AnyObject]?, views: [NSObject : AnyObject]) -> [AnyObject] {
-        return NSLayoutConstraint.constraintsWithVisualFormat(visualFormat, options: nil, metrics: metrics, views: views)
+    class func constraintsWithVisualFormat(visualFormat: String,  metrics: [String : AnyObject]?, views: [String : AnyObject]) -> [AnyObject] {
+        return NSLayoutConstraint.constraintsWithVisualFormat(visualFormat, options: [], metrics: metrics, views: views)
     }
     
-    class func constraintsWithVisualFormat(visualFormat: String,  options: NSLayoutFormatOptions, views: [NSObject : AnyObject]) -> [AnyObject] {
+    class func constraintsWithVisualFormat(visualFormat: String,  options: NSLayoutFormatOptions, views: [String : AnyObject]) -> [AnyObject] {
         return NSLayoutConstraint.constraintsWithVisualFormat(visualFormat, options: options, metrics: nil, views: views)
     }
 }
@@ -25,17 +25,35 @@ extension UIView {
     
     // Given an item, stretches the width and height of the view to the toItem.
     func  stretchToBoundsOfSuperView() -> [NSLayoutConstraint] {
-        var constraints =
-        NSLayoutConstraint.constraintsWithVisualFormat("H:|[item]|", options: nil, metrics: nil, views: ["item" : self]) +
-            NSLayoutConstraint.constraintsWithVisualFormat("V:|[item]|", options: nil, metrics: nil, views: ["item" : self])
+        let constraints =
+        NSLayoutConstraint.constraintsWithVisualFormat("H:|[item]|", options: [], metrics: nil, views: ["item" : self]) +
+            NSLayoutConstraint.constraintsWithVisualFormat("V:|[item]|", options: [], metrics: nil, views: ["item" : self])
         if let superview = self.superview {
             superview.addConstraints(constraints)
         }
-        return constraints as! [NSLayoutConstraint]
+        return constraints 
     }
     
     func alignTopTo(toItem: UIView, padding: CGFloat) -> [NSLayoutConstraint] {
-        var constraint = NSLayoutConstraint(item: self, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: toItem, attribute: NSLayoutAttribute.Top, multiplier: 1.0, constant: padding)
+        let constraint = NSLayoutConstraint(item: self, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: toItem, attribute: NSLayoutAttribute.Top, multiplier: 1.0, constant: padding)
+        self.superview?.addConstraint(constraint)
+        return [constraint]
+    }
+    
+    func alignRightTo(toItem: UIView, padding: CGFloat) -> [NSLayoutConstraint] {
+        let constraint = NSLayoutConstraint(item: self, attribute: NSLayoutAttribute.Right, relatedBy: NSLayoutRelation.Equal, toItem: toItem, attribute: NSLayoutAttribute.Right, multiplier: 1.0, constant: padding)
+        self.superview?.addConstraint(constraint)
+        return [constraint]
+    }
+    
+    func alignBottomTo(toItem: UIView, padding: CGFloat) -> [NSLayoutConstraint] {
+        let constraint = NSLayoutConstraint(item: self, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: toItem, attribute: NSLayoutAttribute.Bottom, multiplier: 1.0, constant: padding)
+        self.superview?.addConstraint(constraint)
+        return [constraint]
+    }
+    
+    func alignLeftTo(toItem: UIView, padding: CGFloat) -> [NSLayoutConstraint] {
+        let constraint = NSLayoutConstraint(item: self, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem: toItem, attribute: NSLayoutAttribute.Left, multiplier: 1.0, constant: padding)
         self.superview?.addConstraint(constraint)
         return [constraint]
     }
@@ -61,73 +79,85 @@ extension UIView {
     }
     
     func stretchToWidthOfSuperView() -> [NSLayoutConstraint] {
-        let constraints = NSLayoutConstraint.constraintsWithVisualFormat("H:|[item]|", options: nil, metrics: nil, views: ["item" : self])
+        let constraints = NSLayoutConstraint.constraintsWithVisualFormat("H:|[item]|", options: [], metrics: nil, views: ["item" : self])
         self.superview?.addConstraints(constraints)
-        return constraints as! [NSLayoutConstraint]
+        return constraints 
     }
     
     func stretchToWidthOfSuperView(padding: CGFloat) -> [NSLayoutConstraint] {
-        let constraints = NSLayoutConstraint.constraintsWithVisualFormat("H:|-padding-[item]-padding-|", options: nil, metrics: ["padding" : padding], views: ["item" : self])
+        let constraints = NSLayoutConstraint.constraintsWithVisualFormat("H:|-padding-[item]-padding-|", options: [], metrics: ["padding" : padding], views: ["item" : self])
         self.superview?.addConstraints(constraints)
-        return constraints as! [NSLayoutConstraint]
+        return constraints 
     }
     
     func stretchToHeightOfSuperView() -> [NSLayoutConstraint] {
-        let constraints = NSLayoutConstraint.constraintsWithVisualFormat("V:|[item]|", options: nil, metrics: nil, views: ["item" : self])
+        let constraints = NSLayoutConstraint.constraintsWithVisualFormat("V:|[item]|", options: [], metrics: nil, views: ["item" : self])
         self.superview?.addConstraints(constraints)
-        return constraints as! [NSLayoutConstraint]
+        return constraints
     }
     
     func stretchToHeightOfSuperView(padding: CGFloat) -> [NSLayoutConstraint] {
-        let constraints = NSLayoutConstraint.constraintsWithVisualFormat("V:|-padding-[item]-padding-|", options: nil, metrics: ["padding" : padding], views: ["item" : self])
+        let constraints = NSLayoutConstraint.constraintsWithVisualFormat("V:|-padding-[item]-padding-|", options: [], metrics: ["padding" : padding], views: ["item" : self])
         self.superview?.addConstraints(constraints)
-        return constraints as! [NSLayoutConstraint]
+        return constraints
     }
     
     func constrainToTopOfSuperView(padding: CGFloat) -> [NSLayoutConstraint] {
-        let constraints = NSLayoutConstraint.constraintsWithVisualFormat("V:|-padding-[item]", options: nil, metrics: ["padding" : padding], views: ["item" : self])
+        let constraints = NSLayoutConstraint.constraintsWithVisualFormat("V:|-padding-[item]", options: [], metrics: ["padding" : padding], views: ["item" : self])
         self.superview?.addConstraints(constraints)
-        return constraints as! [NSLayoutConstraint]
+        return constraints
     }
     
     func constrainToLeftOfSuperView(padding: CGFloat) -> [NSLayoutConstraint] {
-        let constraints = NSLayoutConstraint.constraintsWithVisualFormat("H:|-padding-[item]", options: nil, metrics: ["padding" : padding], views: ["item" : self])
+        let constraints = NSLayoutConstraint.constraintsWithVisualFormat("H:|-padding-[item]", options: [], metrics: ["padding" : padding], views: ["item" : self])
         self.superview?.addConstraints(constraints)
-        return constraints as! [NSLayoutConstraint]
+        return constraints
     }
     
     func constrainToBottomOfSuperView(padding: CGFloat) -> [NSLayoutConstraint] {
-        let constraints = NSLayoutConstraint.constraintsWithVisualFormat("V:[item]-padding-|", options: nil, metrics: ["padding" : padding], views: ["item" : self])
+        let constraints = NSLayoutConstraint.constraintsWithVisualFormat("V:[item]-padding-|", options: [], metrics: ["padding" : padding], views: ["item" : self])
         self.superview?.addConstraints(constraints)
-        return constraints as! [NSLayoutConstraint]
+        return constraints
     }
     
     func constrainToRightOfSuperView(padding: CGFloat) -> [NSLayoutConstraint] {
-        let constraints = NSLayoutConstraint.constraintsWithVisualFormat("H:[item]-padding-|", options: nil, metrics: ["padding" : padding], views: ["item" : self])
+        let constraints = NSLayoutConstraint.constraintsWithVisualFormat("H:[item]-padding-|", options: [], metrics: ["padding" : padding], views: ["item" : self])
         self.superview?.addConstraints(constraints)
-        return constraints as! [NSLayoutConstraint]
+        return constraints
     }
     
     func constrainWidth(width: CGFloat) -> [NSLayoutConstraint] {
-        let constraints = NSLayoutConstraint.constraintsWithVisualFormat("H:[item(width)]", metrics: ["width" : width], views: ["item" : self])
+        let constraints = NSLayoutConstraint.constraintsWithVisualFormat("H:[item(width)]", metrics: ["width" : width], views: ["item" : self]) as! [NSLayoutConstraint]
         self.superview?.addConstraints(constraints)
-        return constraints as! [NSLayoutConstraint]
+        return constraints
     }
     
     func constrainHeight(height: CGFloat) -> [NSLayoutConstraint] {
-        let constraints = NSLayoutConstraint.constraintsWithVisualFormat("V:[item(height)]", metrics: ["height" : height], views: ["item" : self])
+        let constraints = NSLayoutConstraint.constraintsWithVisualFormat("V:[item(height)]", metrics: ["height" : height], views: ["item" : self]) as! [NSLayoutConstraint]
         self.superview?.addConstraints(constraints)
-        return constraints as! [NSLayoutConstraint]
+        return constraints
     }
     
     func constrainWidthTo(toItem: UIView) -> [NSLayoutConstraint] {
-        var constraint = NSLayoutConstraint(item: self, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: toItem, attribute: NSLayoutAttribute.Width, multiplier: 1.0, constant: 0)
+        let constraint = NSLayoutConstraint(item: self, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: toItem, attribute: NSLayoutAttribute.Width, multiplier: 1.0, constant: 0)
+        self.superview?.addConstraint(constraint)
+        return [constraint]
+    }
+    
+    func constrainWidthTo(toItem: UIView, multiplier: CGFloat) -> [NSLayoutConstraint] {
+        let constraint = NSLayoutConstraint(item: self, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: toItem, attribute: NSLayoutAttribute.Width, multiplier: multiplier, constant: 0)
         self.superview?.addConstraint(constraint)
         return [constraint]
     }
     
     func constrainHeightTo(toItem: UIView) -> [NSLayoutConstraint] {
-        var constraint = NSLayoutConstraint(item: self, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: toItem, attribute: NSLayoutAttribute.Height, multiplier: 1.0, constant: 0)
+        let constraint = NSLayoutConstraint(item: self, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: toItem, attribute: NSLayoutAttribute.Height, multiplier: 1.0, constant: 0)
+        self.superview?.addConstraint(constraint)
+        return [constraint]
+    }
+    
+    func constrainHeightTo(toItem: UIView, multiplier: CGFloat) -> [NSLayoutConstraint] {
+        let constraint = NSLayoutConstraint(item: self, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: toItem, attribute: NSLayoutAttribute.Height, multiplier: multiplier, constant: 0)
         self.superview?.addConstraint(constraint)
         return [constraint]
     }
